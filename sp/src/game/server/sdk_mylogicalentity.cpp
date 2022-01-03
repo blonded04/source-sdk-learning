50cf17a86b4089ff3a40cd8da40a9498dda7d7a8
blonded04 <generalretcher@gmail.com>
2022-01-04 00:50:01 +0300

    `CMyLogicalEntity` created, not tested in hammer yet.

---
+++ sp/src/game/server/sdk_mylogicalentity.cpp
@@ -0,0 +1,41 @@
+#include "cbase.h"
+
+class CMyLogicalEntity : public CLogicalEntity {
+	DECLARE_CLASS(CMyLogicalEntity, CLogicalEntity);
+public:
+	CMyLogicalEntity();
+
+	void InputTick(inputdata_t& inputData);
+
+	DECLARE_DATADESC();
+
+private:
+	int m_iThreshold;
+	int m_iCounter;
+
+	COutputEvent m_onThreshold;
+};
+
+LINK_ENTITY_TO_CLASS(my_logical_entity, CMyLogicalEntity);
+
+BEGIN_DATADESC(CMyLogicalEntity)
+	DEFINE_FIELD(m_iCounter, FIELD_INTEGER),
+	
+	DEFINE_KEYFIELD(m_iThreshold, FIELD_INTEGER, "threshold"),
+	
+	DEFINE_INPUTFUNC(FIELD_VOID, "Tick", InputTick),
+
+	DEFINE_OUTPUT(m_onThreshold, "OnThreshold"),
+END_DATADESC()
+
+CMyLogicalEntity::CMyLogicalEntity() {
+	m_iCounter = 0;
+}
+
+void CMyLogicalEntity::InputTick(inputdata_t& inputData) {
+	m_iCounter++;
+	if (m_iThreshold <= m_iCounter) {
+		m_onThreshold.FireOutput(inputData.pActivator, this);
+		m_iCounter = 0;
+	}
+}
