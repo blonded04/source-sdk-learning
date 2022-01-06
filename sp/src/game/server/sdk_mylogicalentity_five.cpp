1304cbf4f741a7e06f94488f343cd0b56cc9e692
blonded04 <generalretcher@gmail.com>
2022-01-06 03:36:13 +0300

    Added one more entity which is a subcase of entity from the tutorial.

---
+++ sp/src/game/server/sdk_mylogicalentity_five.cpp
@@ -0,0 +1,29 @@
+// Actually, `My Stuff/sdk_mylogicalentity_five.cpp` is a subcase of `My Stuff/sdk_mylogicalentity.cpp`
+
+#include "cbase.h"
+#include "memdbgon.h"
+
+class CMyLogicFive : public CLogicalEntity {
+	DECLARE_CLASS(CMyLogicFive, CLogicalEntity);
+public:
+	void InputTick(inputdata_t& inputData);
+
+	DECLARE_DATADESC();
+
+private:
+	int m_iTicks{ 0 };
+};
+
+BEGIN_DATADESC(CMyLogicFive)
+	DEFINE_FIELD(m_iTicks, FIELD_INTEGER),
+
+	DEFINE_INPUTFUNC(FIELD_VOID, "Tick", InputTick),
+END_DATADESC()
+
+void CMyLogicFive::InputTick(inputdata_t& inputData) {
+	m_iTicks++;
+	if (5 <= m_iTicks) {
+		ConColorMsg(Color(255, 0, 0), "\'sdk_mylogicalentity_five\' was triggered!\n");
+		m_iTicks = 0;
+	}
+}
