b4e555700d7d5a9ed350b5f0ca8a9a3b42276c4f
blonded04 <generalretcher@gmail.com>
2022-01-06 04:03:43 +0300

    Clean-up in `sdk_mylogicalentity.cpp`

--- sp/src/game/server/sdk_mylogicalentity.cpp
+++ sp/src/game/server/sdk_mylogicalentity.cpp
@@ -3,15 +3,13 @@
 class CMyLogicalEntity : public CLogicalEntity {
 	DECLARE_CLASS(CMyLogicalEntity, CLogicalEntity);
 public:
-	CMyLogicalEntity();
-
 	void InputTick(inputdata_t& inputData);
 
 	DECLARE_DATADESC();
 
 private:
 	int m_iThreshold;
-	int m_iCounter;
+	int m_iCounter{ 0 };
 
 	COutputEvent m_onThreshold;
 };
@@ -21,16 +19,12 @@ LINK_ENTITY_TO_CLASS(my_logical_entity, CMyLogicalEntity);
 BEGIN_DATADESC(CMyLogicalEntity)
 	DEFINE_FIELD(m_iCounter, FIELD_INTEGER),
 	
-	DEFINE_KEYFIELD(m_iThreshold, FIELD_INTEGER, "threshold"),
+	DEFINE_KEYFIELD(m_iThreshold, FIELD_INTEGER, "Threshold"),
 	
 	DEFINE_INPUTFUNC(FIELD_VOID, "Tick", InputTick),
 
 	DEFINE_OUTPUT(m_onThreshold, "OnThreshold"),
 END_DATADESC()
-
-CMyLogicalEntity::CMyLogicalEntity() {
-	m_iCounter = 0;
-}
 
 void CMyLogicalEntity::InputTick(inputdata_t& inputData) {
 	m_iCounter++;
